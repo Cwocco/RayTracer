7de567153ef3b07e2e6cde554c976761fac4850d
Cwocco <Cwo.payp@gmail.com>
2018-02-15 14:07:52 +0100

    Ntm rotations putain tu rends ouf

--- srcs/vector_rotate.c
+++ srcs/vector_rotate.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 08:48:09 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/02/15 13:55:13 by ada-cunh         ###   ########.fr       */
+/*   Updated: 2018/02/15 14:04:54 by ada-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void rotate_x(t_point *vec, double angle)
 	tmp_y = vec->y;
 	tmp_z = vec->z;
 	vec->y = ((tmp_y * cos(angle)) - (tmp_z * sin(angle)));
-	vec->z = ((vec->y * sin(angle)) + (vec->z * cos(angle)));
+	vec->z = ((tmp_y * sin(angle)) + (tmp_z * cos(angle)));
 }
 
 //Profondeur-> Formule Z
@@ -32,7 +32,7 @@ static void rotate_y(t_point *vec, double angle)
 	tmp_x = vec->x;
 	tmp_z = vec->z;
 	vec->z = ((tmp_z * cos(angle)) - (tmp_x * sin(angle)));
-	vec->x = ((tmp_z * sin(angle)) + (vec->x * cos(angle)));
+	vec->x = ((tmp_z * sin(angle)) + (tmp_x * cos(angle)));
 }
 
 //Hauteur-> formule Y
@@ -44,7 +44,7 @@ static void rotate_z(t_point *vec, double angle)
 	tmp_x = vec->x;
 	tmp_y = vec->y;
 	vec->x = ((tmp_x * cos(angle)) - (tmp_y * sin(angle)));
-	vec->y = ((tmp_x * sin(angle)) + (vec->y * cos(angle)));
+	vec->y = ((tmp_x * sin(angle)) + (tmp_y * cos(angle)));
 }
 
 void vec_unrotate(t_point *vec, t_point rotate)
